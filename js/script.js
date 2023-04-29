9b051828af7fbaaf9a6c2e999ca2329ca41aed58
herndz <hernandesvinicius2005@gmail.com>
2023-04-29 16:30:27 -0300

    Otimização do Design e Media Query

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*let reducedCard = document.querySelector("#reduced-card");
+let distanceCard = document.querySelector("#distance-card");
+let bhaskaraCard = document.querySelector("#bhaskara-card");
+document.querySelector("#reduced-card").addEventListener("mouseover", window.location.href = "/pages/reduced-equation.html");
+document.querySelector("#distance-card").addEventListener("mouseover", window.location.href = "/pages/distance-points.html");
+document.querySelector("#bhaskara-card").addEventListener("mouseover", window.location.href = "/pages/bhaskara.html");
+*/
+
+function link1(){
+   window.location.href = "/pages/reduced-equation.html";
+}
+
+function link2(){
+    window.location.href = "/pages/distance-points.html";
+}
+
+function link3(){
+    window.location.href = "/pages/bhaskara.html";
+}
